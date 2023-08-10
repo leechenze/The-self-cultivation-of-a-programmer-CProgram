ad7c204e1e393ef614b859c6792cd0e738e85134
leechenze <leeczyc@gmail.com>
2023-08-10 23:43:00 +0800

    P006T000000

---
+++ Cpp/001designate&variable&Constant/001designate&variable&Constant/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  001designate&variable&Constant
+//
+//  Created by Lee ChenZe on 8/10/23.
+//
+
+#include <iostream>
+
+
+// using namespace std;    指定缺省的命名空间
+using namespace std;
+
+int main() {
+    cout << endl;
+    /** =========01输出数据========= */
+    // std::cout               可以输出各种类型的数据
+    std::cout << "hello world!\n";
+    std::cout << 100;
+    std::cout << 'L';
+    // 输出一个换行
+    std::cout << std::endl;
+    
+    // <<                      操作符可以拼接多个数据项
+    std::cout << "hello world!\n";
+    std::cout << 100 << 'L' << "lee";
+    std::cout << 'X' << std::endl;
+    std::cout << std::endl;
+    
+    // std::endl               也可以用于换行
+    std::cout << std::endl;
+    
+    // using namespace std;    指定std命名空间
+    cout << "hello world!\n";
+    cout << 100 << endl;
+    cout << 'M' << endl;
+    cout << endl;
+    
+    
+    cout << endl;
+    /** =========02变量========= */
+    // TODO
+    
+    
+}
--- Cpp/helloworld/helloworld/main.cpp
+++ Cpp/helloworld/helloworld/main.cpp
@@ -9,6 +9,9 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "\n                江雪\n";
+    std::cout << "                      柳宗元\n";
+    std::cout << "          千山鸟飞绝，万径人踪灭。\n";
+    std::cout << "          孤舟蓑笠翁，独钓寒江雪。\n";
     return 0;
 }
