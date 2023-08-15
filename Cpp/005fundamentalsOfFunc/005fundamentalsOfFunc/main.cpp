e8b153a23805d7570d2000f54bb1b3e4e40510df
leechenze <leeczyc@gmail.com>
2023-08-15 19:08:55 +0800

    P036T000000

--- Cpp/005fundamentalsOfFunc/005fundamentalsOfFunc/main.cpp
+++ Cpp/005fundamentalsOfFunc/005fundamentalsOfFunc/main.cpp
@@ -2,19 +2,18 @@
 //  main.cpp
 //  005fundamentalsOfFunc
 //
-//  Created by Lee ChenZe on 8/13/23.
+//  Created by Lee ChenZe on 8/15/23.
 //
 
 #include "funcs.hpp"
 #include "tools.hpp"
 
-
 int main(int argc, const char * argv[]) {
     /** =========函数调用========= */
     cout << "man(5,8) = " << maxNum(5,8) << endl;
     cout << endl;
-    
-    
+
+
     /** =========变量的作用域========= */
     func1();
     func1("func1参数变量message");
@@ -28,15 +27,15 @@ int main(int argc, const char * argv[]) {
     func3();
     cout << endl;
 
-    
+
     /** =========函数参数传递========= */
     func4(8, "我是你的上帝");
-    
-    
+
+
     /** =========函数分文件编写========= */
-    
-    
-    
-    
+
+
+
+
     cout<<endl;
 }
