8b04304c966098083857c3dbb839207296b1adfd
leechenze <leeczyc@gmail.com>
2023-08-20 18:36:06 +0800

    P061T000000

---
+++ Cpp/008Array/008Array/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  008Array
+//
+//  Created by Lee ChenZe on 8/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
