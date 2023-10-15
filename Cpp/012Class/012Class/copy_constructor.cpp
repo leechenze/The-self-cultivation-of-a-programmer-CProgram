b8c17b5ad29e3b55957b546e488f05a8cf67793b
leechenze <leeczyc@gmail.com>
2023-10-15 16:11:57 +0800

    P095T000000

---
+++ Cpp/012Class/012Class/copy_constructor.cpp
@@ -0,0 +1,18 @@
+//
+//  copy_constructor.cpp
+//  012Class
+//
+//  Created by Lee ChenZe on 9/4/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+void copy_constructor() {
+    cout << endl;
+
+
+    cout << endl << endl << "==================copy_constructor==================" << endl;
+}
--- Cpp/012Class/012Class/main.cpp
+++ Cpp/012Class/012Class/main.cpp
@@ -12,13 +12,17 @@
 using namespace std;
 
 void class_basic();
+
 void constructor_func();
+
+void copy_constructor();
 
 int main() {
     /** =========类基础(class_basic)========= */
     class_basic();
     /** =========构造函数(constructor_func)========= */
     constructor_func();
-    
+    /** =========拷贝构造函数(copy_constructor)========= */
+    copy_constructor();
 };
 
