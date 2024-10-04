6a6ac7533a8fa87b2d93c9b9da3def0c1bbf2b5b
leechenze <leeczyc@gmail.com>
2024-10-04 16:45:48 +0800

    P110T000001

--- Cpp/013Operator/overloading_parenthesis_operators.cpp
+++ Cpp/013Operator/overloading_parenthesis_operators.cpp
@@ -10,14 +10,18 @@ using namespace std;
 
 // 超女类CGirl5。
 class CGirl5 {
-
+public:
+    void operator()(string str) {
+        cout << "括号运算符重载函数: " << str << endl;
+    }
 };
+
 
 int overloading_parenthesis_operators() {
     cout << endl;
 
-
-
+    CGirl5 cgirl5;
+    cgirl5("超女类5");
 
     cout << endl << endl << "==================overloading_parenthesis_operators==================" << endl;
 }
